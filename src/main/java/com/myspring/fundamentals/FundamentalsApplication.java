dcaa6ab1df531813192da19c741a2b4065cf48f9
PiotrSm <franko102@interia.pl>
2021-03-31 13:19:31 +0200

    Add Swagger update to 3

--- src/main/java/com/myspring/fundamentals/FundamentalsApplication.java
+++ src/main/java/com/myspring/fundamentals/FundamentalsApplication.java
@@ -2,10 +2,8 @@ package com.myspring.fundamentals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class FundamentalsApplication {
 
 	public static void main(String[] args) {
